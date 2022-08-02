ccd060d04050f131b2d2d44e08783be38e506754
seca ospeca <knpitarch@gmail.com>
2022-08-02 19:23:17 -0300

    desafio clase 4

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
-let first1 = prompt("Sabes como se llama el juego famoso de pelea en el que esta scorpion?");
+let precioCompra = Number (prompt('Ingresar Monto Producto en $'));
+let cantidadCuotas = parseInt(prompt('Elegir Plan de Pago/s \n( 1, 2, 3, 6, 9 y 12 pagos)'));
+let precioFinal = 0;
 
-  if (first1 == "si" || first1 == "Si" || first1 == "SI") {
-
-    let name = prompt("Cual es?");
-
-    for (let i = 0; i < 10; i++) {
-
-      while (name != "X") {
-
-        switch (name) {
-
-          case "Mortal Kombat":
-            alert("Exelente ese es!");
-            
-            let first2 = prompt("Y el de hielo se llama Sub-Zero?");
-
-              if (first2 == "si" || first2 == "Si" || first2 == "SI") {
-                alert("Muy bien sabes los personajes principales! Has finalizado!");
-              } 
-              
-              else {
-                alert("Respuesta final?");
-              }
-              
+while (parseInt(precioCompra)) {
+    switch (cantidadCuotas) {
+        case 1: 
+            precioFinal = precioCompra;
+            mensaje(precioFinal, precioCompra, cantidadCuotas);
             break;
-
-          case "Killer Instinct":
-            alert("No, ese no es!");
+        case 2: 
+            precioFinal = precioCompra * 1.0993; 
+            mensaje(precioFinal, precioCompra, cantidadCuotas); 
             break;
-
-          case "Street Fighter":
-            alert("Ese juego no es!");
+        case 3: 
+            precioFinal = precioCompra * 1.1481; 
+            mensaje(precioFinal, precioCompra, cantidadCuotas); 
             break;
-
-          default:
-            alert("Nope, no es ese");
+        case 6: 
+            precioFinal = precioCompra * 1.3024; 
+            mensaje(precioFinal, precioCompra, cantidadCuotas); 
             break;
-        }
-        name = prompt("Ingresa el nombre del juego");    
-      }
-    }
-  }
-  
-  else if (first1 == "no") {
-    alert("Cuando lo sepas podes volver a intentarlo, intenta refrescar la pagina!");
-  }
+        case 9: 
+            precioFinal = precioCompra * 1.5654 
+            mensaje(precioFinal, precioCompra, cantidadCuotas); 
+            break;
+        case 12: 
+            precioFinal = precioCompra * 1.7413 
+            mensaje(precioFinal, precioCompra, cantidadCuotas); 
+            break;
+        default:
+            alert(`El plan en ${cantidadCuotas} pagos no esta disponible`);
+    }       
+        precioCompra = Number(prompt('Ingresar Monto Producto en $'));
+        cantidadCuotas = parseInt(prompt('Elegir Plan de Pago/s \n( 1, 2, 3, 6, 9 y 12 pagos)'));
+        precioFinal = 0;
+}
+
+function valorPagos(precio, cuotas ) {
+    let precioCuota = 0;
+    precioCuota = precio / cuotas;
+    return precioCuota.toFixed(2);
+}
+
+function intereses(precioFinal, precioCompra) {
+    let interes = 0;
+    interes = precioFinal - precioCompra;
+    return interes;
+}
+
+function mensaje(precioFinal, precioCompra, cantidadCuotas) {
+    let mensaje = alert(`El precio final a pagar es de $${(precioFinal).toFixed(2)} en ${cantidadCuotas} pago/s de $${valorPagos(precioFinal, cantidadCuotas)} \n+$${(intereses(precioFinal, precioCompra)).toFixed(2)} de interes`); //dentro del mensaje se ejecutan las funciones de calculo de intereses y valor de pagos
+    return mensaje;
+
+}
