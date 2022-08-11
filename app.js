2b81bfbc8582649e15d76fc8992a11eb5c0a0391
seca ospeca <knpitarch@gmail.com>
2022-08-11 19:51:46 -0300

    primera entrega

--- app.js
+++ app.js
@@ -1,95 +1,104 @@
-const btnEjec = document.querySelector('#btnEjec'); 
-const btnDelet = document.querySelector('#btnDelet'); 
-let listadoJuegos = []; 
-let confirmacion1 = true; 
-let confirmacion2 = true; 
-let idItm = 0; 
-let itemBorrar = 0; 
+let total= 0; 
+let confirmacion0 = true; 
+let cantidad= 0; 
+let codigo = 0; 
+let codigoBorrar = 0; 
+let carritoProductos = JSON.parse(localStorage.getItem('carritoProductos')) || []; 
+let printHtml = document.getElementById('printHtml');
+let productos; 
+const btnDelet = document.querySelector("#btnDelet"); 
+const btnDeletAll = document.querySelector('#btnDeletAll'); 
+const btnCarro1 = document.querySelector('#btnCarro1'); 
+const btnCarro2 = document.querySelector('#btnCarro2'); 
+const btnCarro3 = document.querySelector('#btnCarro3'); 
+const btnCarro4 = document.querySelector('#btnCarro4'); 
+const montoTotal = document.querySelector('#montoTotal'); 
+const cantidadItem = document.querySelector('#casillaCantidad'); 
 
-btnEjec.addEventListener('click', () => { 
+const imprimirEnHtml = (item) => {
+    printHtml.innerHTML = ""; 
+    for(item of item) {
+        productos = document.createElement('tr');
+        productos.innerHTML = `<th scope="row">${item.titulo}</th>
+                            <td>${item.codigo}</td>
+                            <td>$${item.precio}</td>
+                            <td><input id='casillaCantidad' class='text-center' type='number' value='${cantidad}'></input></td>`;
+                                
+        printHtml.appendChild(productos);
+    }
+}
+const filtroPorTitulo = (titulo )=> carritoProductos.filter(producto => producto.titulo === titulo); 
 
-class VideoJuego {
-    constructor(idItm, titulo, plataforma, genero, anio, stock){  
-        this.idItm = idItm;
+const busquedaPorCodigo = (codigo) => carritoProductos.find(producto => producto.codigo === codigo); 
+
+imprimirEnHtml(carritoProductos); 
+
+btnCarro1.addEventListener("click", () => { 
+cantidad = ((filtroPorTitulo('Mortal Kombat 11')).length) + 1; 
+const item1 = new Carrito('Mortal Kombat 11', 1, 2800, cantidad); 
+ingresoCarrito(item1);       
+}); 
+btnCarro2.addEventListener("click", () => { 
+cantidad = ((filtroPorTitulo('Assasins Creed Revelations')).length) + 1; 
+const item2 = new Carrito('Assasins Creed Revelations', 2, 2000, cantidad); 
+ingresoCarrito(item2);           
+}); 
+btnCarro3.addEventListener("click", () => { 
+cantidad = ((filtroPorTitulo('Captain Tsubasa Rise Of New Champions')).length) + 1; 
+const item3 = new Carrito('Captain Tsubasa Rise Of New Champions', 3, 2500, cantidad); 
+ingresoCarrito(item3);              
+}); 
+btnCarro4.addEventListener("click", () => { 
+cantidad = ((filtroPorTitulo('FIFA 23')).length) + 1; 
+const item4 = new Carrito('FIFA 23', 4, 3000, cantidad); 
+ingresoCarrito(item4);             
+}); 
+
+const ingresoCarrito = (item) => { 
+carritoProductos.push(item);
+localStorage.setItem('carritoProductos', JSON.stringify(carritoProductos)); 
+
+location.reload();
+
+}
+
+class Carrito {
+    constructor(titulo, codigo, precio, cantidad) { 
         this.titulo = titulo;
-        this.plataforma = plataforma;
-        this.genero = genero;
-        this.anio = anio;
-        this.stock = stock;
-    }
-    printConsole(){ 
-        console.log(`%cITEM #${this.idItm}`,'color: black; font-weight: bold; background:#0f0;'); 
-        console.log(`TÍTULO: ${this.titulo}\nPLATAFORMA: ${this.plataforma}\nGENERO: ${this.genero}\nAÑO: ${this.anio}\nSTOCK: ${this.stock}\n----------------------------\n`);
-    
-    }
+        this.codigo = codigo;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }  
 }
 
-const ingresarDatos = () => {
+btnDelet.addEventListener("click", () => { 
 
-    let titulo = prompt('Ingrese nombre videojuego.', 'COUNTER-STRIKE 1.6'); 
-    titulo === null ? titulo ='SIN NOMBRE' : titulo = titulo.toUpperCase();
-    
-    let plataforma = prompt('Ingrese plataforma videojuego.', 'PC'); 
-    plataforma === null ? plataforma ='SIN PLATAFORMA' : plataforma = plataforma.toUpperCase();
+    if(carritoProductos.length > 0) {
+        do {
+            codigoBorrar = parseInt(prompt("Ingrese el código del producto a borrar")); 
+            if (isNaN(codigoBorrar)) { 
+                confirmacion0 = false;
+            } else if (busquedaPorCodigo(codigoBorrar) !== undefined) { 
+                confirmacion0 = confirm(`Desea quitar el código #${codigoBorrar} del listado?`); 
+                carritoProductos = JSON.parse(localStorage.getItem('carritoProductos')); 
+                let indexItemBorrar = carritoProductos.findIndex(producto => producto.codigo === codigoBorrar); 
+                carritoProductos.splice(indexItemBorrar, 1); 
+                localStorage.setItem('carritoProductos', JSON.stringify(carritoProductos)); 
+                location.reload(); 
 
-    let genero = prompt('Ingrese genero videojuego.', 'FPS'); 
-    genero === null ? genero = 'SIN GENERO' : genero = genero.toUpperCase(); 
-
-    let anio = parseInt(prompt('Ingrese año videojuego.', 1999)); 
-    isNaN(anio) ? anio = 0 : anio = anio; 
-
-    let stock = parseInt(prompt('Ingrese stock videojuego.', 10)); 
-    isNaN(stock) ? stock = 0 : stock = stock; 
-    
-
-    return {titulo , plataforma, genero, anio, stock} ; 
-}
-
-
-do { 
-    const {titulo, plataforma, genero, anio, stock} = ingresarDatos(); 
-
-    if(titulo === '' ) { 
-    console.warn('Error: Item no valido - Debe asignar un Nombre al item.'); 
-
-    } else { 
-
-    idItm++ 
-    let itemGame = new VideoJuego(idItm, titulo, plataforma, genero, anio , stock); 
-    listadoJuegos.push(itemGame);
-    itemGame.printConsole(); 
-    }
-    
-   confirmacion1 = confirm("Desea ingresar un nuevo item?"); 
-
-} while(confirmacion1);
-    console.log('%cLISTADO DE ITEMS INGRESADOS', 'color: white; font-size: 16px; font-weight: bold; background: blue;');
-    console.table(listadoJuegos); 
-
-}); 
-
-btnDelet.addEventListener('click', () => { 
-
-    do{
-        itemBorrar = parseInt (prompt("Ingrese el número de item a borrar")); 
-
-        const items = listadoJuegos.filter( item =>{ 
-            return item.idItm === itemBorrar;
-        })
-
-        if(isNaN(itemBorrar)) { 
-            confirmacion2 = false
-        } else if (items.length > 0) { 
-            confirmacion2 = confirm(`Desea quitar item #${itemBorrar} del listado?`); 
-            switch (confirmacion2) { 
-                case true:
-                let i = itemBorrar - 1; 
-                delete listadoJuegos[i]; 
-                console.log('%cLISTADO DE ITEMS INGRESADOS', 'color: white; font-size: 16px; font-weight: bold; background: blue;'); 
-                console.table(listadoJuegos); 
+            } else {
+                alert(`El item #${codigoBorrar} no existe en el listado`); 
             }
-        } else {
-            alert(`El item #${itemBorrar} no existe en el listado`); 
-        }
-    } while(confirmacion2); 
+    
+        } while (confirmacion0); // 
+    }
 }); 
+
+btnDeletAll.addEventListener("click", () => { 
+    if(carritoProductos.length >0) {
+            carritoProductos = [];
+            localStorage.clear();
+            location.reload();
+        }
+    
+});
